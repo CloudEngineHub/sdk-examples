a1d2fd381a8abf5825f90b5bd44286433501d1b4
Pekka Rantalankila <pekka.rantalankila@gmail.com>
2026-05-18 16:00:53 +0300

    plot_positions.py: Add --3d option.

--- cpp/offline/plot_positions.py
+++ cpp/offline/plot_positions.py
@@ -20,13 +20,24 @@ def read_data(fn):
         pos.append([o[POSITION][c] for c in 'xyz'])
     return np.array(pos)
 
+def set_axes_equal_3d(ax):
+    from mpl_toolkits.mplot3d.axes3d import Axes3D
+    assert isinstance(ax, Axes3D)
+    limits = np.array([ax.get_xlim3d(), ax.get_ylim3d(), ax.get_zlim3d()])  # type: ignore[attr-defined]
+    center = limits.mean(axis=1)
+    half_range = (limits[:, 1] - limits[:, 0]).max() / 2
+    ax.set_xlim3d(center[0] - half_range, center[0] + half_range)
+    ax.set_ylim3d(center[1] - half_range, center[1] + half_range)
+    ax.set_zlim3d(center[2] - half_range, center[2] + half_range)
+
 if __name__ == '__main__':
     import argparse
     import matplotlib.pyplot as plt
 
     p = argparse.ArgumentParser(__doc__)
     p.add_argument('jsonl', help='VIO output file')
-    p.add_argument('-image', help='Save image to this path instead of showing the plot')
+    p.add_argument('--image', help='Save image to this path instead of showing the plot')
+    p.add_argument('--3d', action='store_true', dest='plot3d', help='Plot in 3D (interactively rotatable)')
     args = p.parse_args()
 
     pos = read_data(args.jsonl)
@@ -34,8 +45,19 @@ if __name__ == '__main__':
         print("No data to plot.")
         sys.exit()
 
-    plt.plot(pos[:,0], pos[:,1])
-    plt.axis('equal')
+    if args.plot3d:
+        from mpl_toolkits.mplot3d.axes3d import Axes3D
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        assert isinstance(ax, Axes3D)
+        ax.plot(pos[:,0], pos[:,1], pos[:,2])
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        set_axes_equal_3d(ax)
+    else:
+        plt.plot(pos[:,0], pos[:,1])
+        plt.axis('equal')
 
     if args.image:
         plt.savefig(args.image)
